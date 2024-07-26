217004a74ecbb2f1cc41eebbcaecbe32e0d02271
SorSerafim <ricardoserafim1920@gmail.com>
2024-07-25 21:59:06 -0300

    Result model created

--- src/backoffice/controllers/customer.controller.ts
+++ src/backoffice/controllers/customer.controller.ts
@@ -7,35 +7,34 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
+import { Customer } from '../models/customer.models';
+import { Result } from '../models/result.model';
 
 // localhost:3000/v1/customers
 @Controller('v1/customers')
 export class CustomerController {
   @Get()
   get() {
-    return 'Obter os clientes';
+    return new Result(null, true, [], null);
   }
 
   @Get(':document')
-  getById(@Param('document') document) {
-    return 'Obter os clientes' + document;
+  getById(@Param('document') document: string) {
+    return new Result(null, true, {}, null);
   }
 
   @Post()
-  post(@Body() body) {
-    return body;
+  post(@Body() body: Customer) {
+    return new Result('Cliente criado com sucesso!', true, body, null);
   }
 
   @Put(':document')
   put(@Param('document') document, @Body() body) {
-    return {
-      customer: document,
-      data: body,
-    };
+    return new Result('Cliente alterado com sucesso!', true, body, null);
   }
 
   @Delete(':document')
   delete(@Param('document') document) {
-    return 'Remover um cliente' + document;
+    return new Result('Cliente removido com sucesso!', true, null, null);
   }
 }
