83778ea5d98bd397ee5295c7b718ae72bcc6a825
SorSerafim <ricardoserafim1920@gmail.com>
2024-08-12 23:45:43 -0300

    Create customer.contracts

---
+++ src/backoffice/contracts/customer.contracts.ts
@@ -0,0 +1,18 @@
+import { Flunt } from 'src/utils/flunt';
+import { Contract } from './contract';
+import { Customer } from '../models/customer.models';
+
+export class CreateCustomerContract implements Contract {
+  errors: any[];
+
+  validate(model: Customer): boolean {
+    const flunt = new Flunt();
+
+    flunt.hasMinlen(model.name, 5, 'Nome inválido!');
+    flunt.isEmail(model.email, 'E-mail inválido!');
+    flunt.isFixedLen(model.document, 11, 'CPF inválido!');
+    flunt.hasMinlen(model.password, 6, 'Senha inválida!');
+
+    this.errors = flunt.errors;
+  }
+}
